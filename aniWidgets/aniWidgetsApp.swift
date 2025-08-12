e135a2748a4c66ad7745d2981deaf92f931334b7
frowing37 <froward37@gmail.com>
2025-08-12 15:53:12 +0300

    Initial Commit

---
+++ aniWidgets/aniWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  aniWidgetsApp.swift
+//  aniWidgets
+//
+//  Created by Emre Can Mece on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct aniWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aniWidgetsTests/aniWidgetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  aniWidgetsTests.swift
+//  aniWidgetsTests
+//
+//  Created by Emre Can Mece on 12.08.2025.
+//
+
+import Testing
+@testable import aniWidgets
+
+struct aniWidgetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
